53fef7a3382f8695733d4b019029debeba6ca171
walid <w.benchamekh@roam-smart.com>
2018-09-28 15:58:11 +0100

    fix foreign key issue with introducing of the app_context functionality

--- config.py
+++ config.py
@@ -2,13 +2,16 @@ class Config(object):
     DEBUG = True
     TESTING = False
     SQLALCHEMY_DATABASE_URI = 'mysql://root:Walid78!@localhost/steeringiot'
+    STATIC_FOLDER = '/home/walid/devops/steeringiot/static/data'
 
 class Production(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql://root:Walid78!@192.168.43.201/steeringiot'
+    STATIC_FOLDER = '/home/walid/devops/steeringiot/static/data'
 
 class Development(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql://root:Walid78!@localhost/steeringiot'
+    STATIC_FOLDER = '/home/walid/devops/steeringiot/static/data'
 
 class TestingConfig(Config):
     TESTING = True
